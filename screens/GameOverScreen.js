2cc678d78a41bc079d087f622946905df4492dfd
CAM603 <cameronhawley@comcast.net>
2020-03-27 16:24:48 -0400

    ADD image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>GAME OVER</Text>
-      <Text>Number of rounds: {props.rounds}</Text>
-      <Text>Number was: {props.userNumber}</Text>
-      <Button title="New Game" onPress={props.newGame}/>
+      <TitleText>GAME OVER</TitleText>
+      <View style={styles.imageConatainer}>
+        <Image 
+          source={require('../assets/success.png')} 
+          style={styles.image} 
+          resizeMode="cover"
+          // For web images... fadeDuration={1000}
+        />
+      </View>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>Your phone needed <Text style={styles.highlight}>{props.rounds}</Text> rounds to guess the number <Text style={styles.highlight}>{props.userNumber}</Text></BodyText>
+        <Button title="New Game" onPress={props.newGame}/>
+      </View>
     </View>
   )
 }
@@ -17,6 +29,32 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  image: {
+    width: '100%%',
+    height: '100%'
+  },
+  imageConatainer: {
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: 'black',
+    height: 300,
+    width: 300,
+    overflow: 'hidden',
+    marginVertical: 30
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: 'open-sans-bold',
+    
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: 20
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginVertical: 15
   }
 })
 
